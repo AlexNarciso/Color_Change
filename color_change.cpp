3ff30398c24e5bce1c8f393fcc85eec1431541eb
AlexNarciso <alexpantera31@gmail.com>
2024-02-12 16:53:49 +0000

    feat: add different colors when pressed certain keys

--- color_change.cpp
+++ color_change.cpp
@@ -2,7 +2,54 @@
 
 int main(){
 
+//color change nº
+int n=0;
 
+//window size
+int width=1280,
+height=720;
+
+//Background color
+Color color = WHITE;
+
+InitWindow(width,height,"Color_change");
+
+
+while(WindowShouldClose() == false){
+BeginDrawing();
+ClearBackground(color);
+
+if(IsKeyPressed(KEY_R)){
+  color=RED;
+}
+
+if(IsKeyPressed(KEY_B)){
+    if(n==0){
 
     
+        color=BLUE;
+        n++;
+    }else{
+        color=BLACK;
+        n=0;
+    }
+}
+
+if(IsKeyPressed(KEY_W)){
+  color=WHITE;
+}
+
+if(IsKeyPressed(KEY_G)){
+  color=GREEN;
+}
+
+if(IsKeyPressed(KEY_Y)){
+  color=YELLOW;
+}
+
+
+EndDrawing();
+}
+
+
 }
